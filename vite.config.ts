23583e6a73e9fb9cd1f5ce22c4d59668a4bd0a9c
Matej Koncal <matej.koncal@gmail.com>
2025-06-30 22:33:30 +0200

    Add personal configs (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
